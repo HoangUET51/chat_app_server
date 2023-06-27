f4d82d77879f4a97234d1d5b8837e39fcafe8351
hoang <vuvanhoanguet51@gmail.com>
2023-06-27 23:39:00 +0700

    refs #2 Send mail, update feat

--- src/helpers/user.helper.ts
+++ src/helpers/user.helper.ts
@@ -9,3 +9,15 @@ export const hashPassword = (password: string) => {
 export const checkPassword = (password: string, hashPassword: string) => {
   return bcrypt.compare(password, hashPassword);
 };
+
+export const randomPassword = () => {
+  const length = 12;
+  const charset =
+    "@#$&*0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ@#$&*0123456789abcdefghijklmnopqrstuvwxyz";
+  let password = "";
+
+  for (let i = 0, n = charset.length; i < length; i++) {
+    password += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return password;
+};
--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,6 +1,9 @@
+import { MAIL_ACTION } from "@/constants/sendMail.const";
 import { Users } from "@/entities/user";
-import { hashPassword } from "@/helpers/user.helper";
+import { hashPassword, randomPassword } from "@/helpers/user.helper";
 import { UserModel } from "@/models/user.model";
+import { MailInfo, sendMails } from "@/services/sendMail.service";
+import { sign, verify } from "jsonwebtoken";
 class _UserRepository {
   async getUser(email: string) {
     return Users.findOne({ email });
@@ -9,6 +12,7 @@ class _UserRepository {
   async createOrEdit(user: UserModel) {
     try {
       if (user._id) {
+        const hassPassword = hashPassword(user.password);
         return Users.updateOne(
           { _id: user._id },
           {
@@ -16,23 +20,98 @@ class _UserRepository {
               fullName: user.fullName,
               gender: user.gender,
               address: user.address,
+              password: hassPassword,
             },
           },
         );
       } else {
         const hassPassword = hashPassword(user.password);
-        const newUser = new Users({
-          fullName: user.fullName,
-          email: user.email,
-          password: hassPassword,
-          address: user.address,
-          phone: user.phone,
-          gender: user.gender,
-          avatar: user?.avatar ?? null,
-        });
-        const result = await newUser.save();
-        return result;
+        const token = sign(
+          {
+            email: user.email,
+            fullName: user.fullName,
+            avatar: user?.avatar ?? null,
+            address: user.address,
+            gender: user.gender,
+            phone: user.phone,
+            password: hassPassword,
+          },
+          `${process.env.TOKEN_KEY}`,
+          { expiresIn: "1d" },
+        );
+
+        const mailAction: MailInfo = {
+          mailAction: MAIL_ACTION.ACTIVATE_ACCOUNT,
+          toEmail: user.email,
+          toUser: user.fullName,
+          others: token,
+        };
+
+        sendMails(mailAction);
+        return token;
       }
+    } catch (error) {
+      return null;
+    }
+  }
+
+  async activateAccount(token: string | null) {
+    try {
+      if (!token) return;
+
+      const user = (await verify(
+        token,
+        process.env.TOKEN_KEY ?? "",
+      )) as UserModel;
+
+      if (!user) {
+        return;
+      }
+
+      const newUser = new Users({
+        fullName: user.fullName,
+        email: user.email,
+        password: user.password,
+        address: user.address,
+        phone: user.phone,
+        gender: user.gender,
+        avatar: user?.avatar ?? null,
+      });
+
+      const result = await newUser.save();
+      return result;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  async forgetPassword(email: string) {
+    try {
+      const user = await Users.findOne({ email });
+      if (!user) {
+        return;
+      }
+
+      const newPassword = randomPassword();
+      const hassPassword = hashPassword(newPassword);
+
+      const mailAction: MailInfo = {
+        mailAction: MAIL_ACTION.FORTGET_PASSWORD,
+        toEmail: email,
+        toUser: user.fullName,
+        others: newPassword,
+      };
+
+      sendMails(mailAction);
+      return Users.updateOne(
+        { email },
+        {
+          $set: {
+            password: hassPassword,
+          },
+        },
+      );
     } catch (error) {
       return null;
     }
